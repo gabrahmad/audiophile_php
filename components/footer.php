7384e7f244d5c31fea6d4a5df117cded877d4b8d
gabrahmad <ahmedgabr1987@gmail.com>
2021-06-26 03:17:08 +0200

    add footer

--- components/footer.php
+++ components/footer.php
@@ -1,2 +1,35 @@
-<script src="javascript/main.js" lang="javascript"></script>
+
+<div class=" has-background-black has-text-centered has-text-white">
+    <nav class="navbar is-black  container" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand ">
+        <a class="navbar-item title" href="">
+          audophile
+        </a>
+
+        
+      </div>
+
+      <div id="navbarfooter" class="navbar-menu is-active has-background-black ">
+        <div class="navbar-end  ml-6 is-uppercase">
+          <a class="navbar-item">
+            Home
+          </a>
+
+          <a class="navbar-item">
+            Headphones
+          </a>
+          <a class="navbar-item">
+            Speakers
+          </a>
+          <a class="navbar-item">
+            Earphones
+          </a>
+        </div>
+
+      </div>
+
+    </nav>
+          <p class="p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia asperiores suscipit fugiat maxime. Alias nisi, quis officiis consequatur tempore, explicabo natus obcaecati dolores, eveniet architecto vitae accusantium adipisci recusandae aliquid.</p>
+
+  </div><script src="javascript/main.js" lang="javascript"></script>
 </body>
--- index.php
+++ index.php
@@ -12,6 +12,20 @@ $categories = [
 <?php 
 include ("components/header.php")
 ?>
+
+<!--start of main product -->
+  <div class="  hero is-halfheight section " id="mainHero">
+    <div class=" has-text-white">
+      <h4 class="is-uppercase is-size-4-desktop is-size-6 ">new product</h4>
+      <h1 class="is-uppercase title has-text-white is-size-1-desktop is-size-3 py-1">XX99 Mark II</br> Headphones</h1>
+      <p class="is-smaller">Experience natural, lifelike audio and exceptional</br> build quality made for the passionate music</br> enthusiast.</p>
+      <a class="button is-uppercase mt-5 has-text-white has-text-weight-bold p-4" style="background-color: orange;">see product</a>
+    </div>
+
+  </div>
+  <!--end of main product -->
+
+  
   <!-- start of categories --> 
   <div class="section columns is-8 is-variable is-uppercase has-text-centered m-4">
       <?php 
@@ -41,7 +55,7 @@ include ("components/header.php")
 
 
 
-  
+
 <?php
   include ("components/footer.php");
 ?>
